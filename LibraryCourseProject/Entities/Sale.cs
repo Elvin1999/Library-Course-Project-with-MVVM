a119de04389b67b4b4ac8dd6f9f68d55e6d7f8fb
camalzade_elvin <camalzade_elvin@outlook.com>
2019-05-28 15:37:51 +0500

    Some additional thing for SaleViewModel

--- LibraryCourseProject/Entities/Sale.cs
+++ LibraryCourseProject/Entities/Sale.cs
@@ -20,5 +20,25 @@ namespace LibraryCourseProject.Entities
         public double SalePrice { get; set; }
         public DateTime SaleDateTime { get; set; } = DateTime.Now;
         public string Note { get; set; } = "empty";
+
+        public Sale Clone()
+        {
+            Sale sale = new Sale()
+            {
+                 Book=this.Book,
+                  Book_Id=this.Book_Id,
+                   Client=this.Client,
+                    Client_Id=this.Client_Id,
+                     Id=this.Id,
+                      No=this.No,
+                       Note=this.Note,
+                        RealPrice=this.RealPrice,
+                         SaleDateTime=this.SaleDateTime,
+                          SalePrice=this.SalePrice,
+                           User=this.User,
+                            User_Id=this.User_Id
+            };
+            return sale;
+        }
     }
 }
--- LibraryCourseProject/ViewModels/SaleViewModel.cs
+++ LibraryCourseProject/ViewModels/SaleViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using LibraryCourseProject.Entities;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,57 @@ namespace LibraryCourseProject.ViewModels
 {
    public class SaleViewModel:BaseViewModel
     {
+        //public AddCommand AddCommand => new AddCommand(this);
+        //public DeleteCommand DeleteCommand => new DeleteCommand(this);
+        //public UpdateCommand UpdateCommand => new UpdateCommand(this);
+        public ClientViewModel ClientViewModel { get; set; }
+        private ObservableCollection<Sale> allSales;
+        public ObservableCollection<Sale> AllSales
+        {
+            get
+            {
+                return allSales;
+            }
+            set
+            {
+                allSales = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(AllSales)));
+            }
+        }
+        public SaleViewModel()
+        {
+            CurrentSale = new Sale();
+        }
+        private Sale currentSale;
+        public Sale CurrentSale
+        {
+            get
+            {
+                return currentSale;
+            }
+            set
+            {
+                currentSale = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(CurrentSale)));
+            }
+        }
 
+        private Sale selectedSale;
+        public Sale SelectedSale
+        {
+            get
+            {
+                return selectedSale;
+            }
+            set
+            {
+                selectedSale = value;
+                if (value != null)
+                {
+                    CurrentSale = SelectedSale.Clone();
+                }
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(SelectedSale)));
+            }
+        }
     }
 }
