7d9154c35aacff7fe57d7f0082039574c430c4f2
camalzade_elvin <camalzade_elvin@outlook.com>
2019-06-16 19:38:47 +0500

    second time addition . . .

--- LibraryCourseProject/App.xaml.cs
+++ LibraryCourseProject/App.xaml.cs
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace LibraryCourseProject
+namespace LibraryCourseProject 
 {
     /// <summary>
     /// Interaction logic for App.xaml
